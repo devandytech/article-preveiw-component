625ef91d4443a8d4c371a0eec68e154ceaf80cc1
Oseremen Godstime <152567354+devandytech@users.noreply.github.com>
2025-10-20 21:55:19 +0100

    Update script.js
    Add event listeners for DOM elements on page load.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+  const activeWrapper = document.querySelector('.active-state');
+  const childWrapper = document.querySelector('.child-wrapper')
+  const shareBtn = document.querySelector('.share-icon');
+  const profileImg = document.getElementById('second-img');
+  const childDiv = document.querySelector('.child-div')
+  
+  shareBtn.addEventListener('click', activeState);
+  
+  function activeState() {
+    
+  }
+})
